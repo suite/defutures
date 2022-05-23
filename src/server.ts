537ad52ad469830131e0231e8f3541a3c43a0f53
Matt <gr8bitmail@gmail.com>
2022-05-23 01:31:27 -0500

    trying to fix cors

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3001;
 const CORS_ORIGIN = process.env.ORIGIN as string || 'http://localhost:3000';
  
 app.use(express.json());
-app.use(cors({ credentials: true, origin: '*' }));
+app.use(cors({ credentials: true, origin: CORS_ORIGIN }));
 app.use(cookieParser());
 
 /* THOUGHTS:
