5c307c3539697b5ea89c46a243b15f82553c6948
Matt <gr8bitmail@gmail.com>
2024-01-20 18:29:59 -0700

    change updatepick logic

--- src/queries/updatePick.ts
+++ src/queries/updatePick.ts
@@ -16,18 +16,25 @@ export default async function updatePick(
             throw new Error('Pick not found');
         }
 
+        const winningSelectionIdsStringified = winningSelectionIds.map(id => JSON.stringify(id));
+
         // Update the "winner" field for winning teams
         pick.selections.forEach(selection => {
             selection.teams.forEach(team => {
-                if (winningSelectionIds.includes(team._id)) {
+                if (winningSelectionIdsStringified.includes(JSON.stringify(team._id))) {
                     team.winner = true;
+                } else {
+                    team.winner = false;
                 }
             });
         });
 
+
+
         // Update points and tieBreakerPoints for placed bets
         pick.placedBets.forEach(bet => {
-            bet.points += bet.pickedTeams.filter(teamId => winningSelectionIds.includes(teamId)).length;
+            bet.points = bet.pickedTeams.filter(teamId => 
+                winningSelectionIdsStringified.includes(JSON.stringify(teamId))).length;
 
             if (typeof tiebreaker !== 'undefined') {
                 bet.tieBreakerPoints = tiebreaker - Math.abs(bet.tieBreaker - tiebreaker);
